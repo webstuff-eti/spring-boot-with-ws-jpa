539c0c959fee09813d9fa39de030ec4b53d93067
Tiago Tibães <tibaestiago@gmail.com>
2019-03-06 20:09:54 -0300

    gerando classes para customer

--- crm/src/main/java/br/eti/webstuff/soap/crm/generated/jaxb/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
-// Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2019.03.04 às 06:13:49 PM BRT 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://spring.io/guides/gs-producing-web-service", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package br.eti.webstuff.soap.crm.generated.jaxb;
--- crm/src/main/java/br/eti/webstuff/soap/crm/generated/jaxb/teste.java
+++
@@ -1,5 +0,0 @@
-package br.eti.webstuff.soap.crm.generated.jaxb;
-
-public class teste {
-
-}
