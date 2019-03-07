4d605f2384e9f59ce7091754bae64ed3f77230c9
Tiago Tibães <tibaestiago@gmail.com>
2019-03-06 23:18:46 -0300

    Implementação necessárias para gerar wsdl

--- crm/src/main/java/br/eti/webstuff/soap/crm/config/WebServiceConfig.java
+++ crm/src/main/java/br/eti/webstuff/soap/crm/config/WebServiceConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @Configuration
 @EnableWs
@@ -22,5 +26,24 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 		
 		return new ServletRegistrationBean(messageDispatcherServlet, "/ws/*");
 	}
-
+	
+	
+	/*  Configurações necessárias para gerar o WSDL   */
+	@Bean
+    public XsdSchema customerSchema() {
+    	return new SimpleXsdSchema(new ClassPathResource("customer-information.xsd"));
+    }
+	
+	@Bean
+	public DefaultWsdl11Definition defaultWsdl11Definition(XsdSchema customerSchema) {
+		
+		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+		
+		definition.setPortTypeName("CustomerPort");
+		definition.setTargetNamespace("http://www.webstuff.eti.br/soap/crm/generated/jaxb/customer");
+		definition.setLocationUri("/ws");
+		definition.setSchema(customerSchema);
+		
+		return definition;
+	}
 }
